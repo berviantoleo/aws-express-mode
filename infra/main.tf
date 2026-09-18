d7e8b8512b223b78c87cd04c84cb10575918d4ef
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2026-09-18 18:35:56 +0700

    infra sample

---
+++ infra/main.tf
@@ -0,0 +1,83 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+# 1. Create IAM roles for ECS Express Gateway
+resource "aws_iam_role" "execution" {
+  name = "ecs-express-gateway-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "execution" {
+  role       = aws_iam_role.execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role" "infrastructure" {
+  name = "ecs-express-gateway-infrastructure-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "infrastructure" {
+  role       = aws_iam_role.infrastructure.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSInfrastructureRoleforExpressGatewayServices"
+}
+
+# 2. Create ECS Express Gateway Service
+
+resource "aws_ecs_express_gateway_service" "apisample" {
+  execution_role_arn      = aws_iam_role.execution.arn
+  infrastructure_role_arn = aws_iam_role.infrastructure.arn
+  health_check_path       = "/health"
+
+
+  primary_container {
+    container_port = 8080
+    image = "ghcr.io/berviantoleo/aws-express-mode/apisample:latest"
+  }
+
+  depends_on = [
+    aws_iam_role.execution,
+    aws_iam_role.infrastructure,
+  ]
+}
+
+# 3. Output the URL of the ECS Express Gateway Service
+
+output "apisample_url" {
+  value = aws_ecs_express_gateway_service.apisample.ingress_paths
+}
